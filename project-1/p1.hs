5d3ee2a960a2f1471972b651dd1e5d3f14f2c7a1
Herbert Aruya <haruyacs@gmail.com>
2019-10-01 06:32:56 -0500

    completed (hopefully)

--- project-1/p1.hs
+++ project-1/p1.hs
@@ -27,44 +27,44 @@ data ABE where
 -- Evaluation Functions
 
 evalM :: ABE -> (Maybe ABE)
-evalM (Num n) = Just n
-evalM (Boolean b) = Just b
-evalM (Plus l r) = do { l_prime <- evalM(l);
-                        r_prime <- evalM(r); 
-                        return (l_prime + r_prime) }
-evalM (Minus l r) = do { l_prime <- evalM(l); 
-                         r_prime <- evalM(r); 
+evalM (Num n) = Just (Num n)
+evalM (Boolean b) = Just (Boolean b) 
+evalM (Plus l r) = do { Num l_prime <- evalM(l);
+                        Num r_prime <- evalM(r); 
+                        return (Num(l_prime + r_prime)) }
+evalM (Minus l r) = do { Num l_prime <- evalM(l); 
+                         Num r_prime <- evalM(r); 
                          if (r_prime > l_prime) then Nothing
-                           else return (l_prime-r_prime) }
-evalM (Mult l r) = do { l_prime <- evalM(l);
-                        r_prime <- evalM(r);
+                           else return (Num (l_prime-r_prime)) }
+evalM (Mult l r) = do { Num l_prime <- evalM(l);
+                        Num r_prime <- evalM(r);
                         let potential_eval = l_prime*r_prime in
-                         if(0 > potential_eval) then Nothing else return potential_eval; }
-evalM (Div l r) = do{ l_prime <- evalM(l);
-                      r_prime <- evalM(r); 
+                         if(0 > potential_eval) then Nothing else return (Num (potential_eval)); }
+evalM (Div l r) = do{ Num l_prime <- evalM(l);
+                      Num r_prime <- evalM(r); 
                       if 0 == r_prime then Nothing else
                          let potential_eval = (div l_prime r_prime) in 
-                            if (0 > potential_eval) then Nothing else return potential eval; }
-evalM (And l r) = do { l_prime <- evalM(l);
-                       r_prime <- evalM(r); 
-                       return (l_prime && r_prime) }
-evalM (Leq l r) = do { l_prime <- evalM(l); 
-                       r_prime <- evalM(r); 
-                       return (l_prime <= r_prime) }
-evalM (IsZero t) = do { term <- evalM(t); 
-                        return (0 == term) }
-evalM (If c t e) = do { condition <- evalM(c);
-                        if c then evalM(t) else evalM(c) }
+                            if (0 > potential_eval) then Nothing else return (Num (potential_eval)); }
+evalM (And l r) = do { Boolean l_prime <- evalM(l);
+                       Boolean r_prime <- evalM(r); 
+                       return (Boolean (l_prime && r_prime)) }
+evalM (Leq l r) = do { Num l_prime <- evalM(l); 
+                       Num r_prime <- evalM(r); 
+                       return (Boolean (l_prime <= r_prime)) }
+evalM (IsZero t) = do { Num term <- evalM(t); 
+                        return (Boolean (0 == term)) }
+evalM (If c t e) = do { Boolean condition <- evalM(c);
+                        if condition then evalM(t) else evalM(c) }
 
 
 evalErr :: ABE -> (Maybe ABE)
-evalErr _ = Nothing -- Replace this with your interpreter
+evalErr expression  = evalM(expression) -- Replace this with your interpreter
 
 -- Type Derivation Function
 
 typeofM :: ABE -> Maybe TABE
-typeofM (Num n) = TNum
-typeofM (Boolean b) = TBool
+typeofM (Num n) = Just TNum
+typeofM (Boolean b) = Just TBool
 typeofM (Plus l r) = do { TNum <- typeofM(l);
                           TNum <- typeofM(r); 
                           return TNum }
@@ -94,78 +94,85 @@ typeofM (If c t e) = do { TBool <- typeofM(c);
 -- Combined interpreter
 
 evalTypeM :: ABE -> Maybe ABE
-evalTypeM (Num n) = Just n
-evalTypeM (Boolean b) = Just b
+evalTypeM (Num n) = Just (Num n)
+evalTypeM (Boolean b) = Just (Boolean b)
 evalTypeM (Plus l r) = do { TNum <- typeofM(l); 
                             TNum <- typeofM(r); 
                             
-                            l_prime <- evalTypeM(l); 
-                            r_prime <- evalTypeM(r);
-                            return (l_prime + r_prime) }
+                            Num l_prime <- evalTypeM(l); 
+                            Num r_prime <- evalTypeM(r);
+                            return (Num (l_prime + r_prime)) }
 evalTypeM (Minus l r) = do { TNum <- typeofM(l); 
                              TNum <- typeofM(r); 
                             
-                             l_prime <- evalTypeM(l); 
-                             r_prime <- evalTypeM(r);
+                             Num l_prime <- evalTypeM(l); 
+                             Num r_prime <- evalTypeM(r);
                              if r_prime > l_prime then Nothing
-                                 else return (l_prime - r_prime) }
+                                 else return (Num (l_prime - r_prime)) }
 evalTypeM (Mult l r) = do { TNum <- typeofM(l);
                             TNum <- typeofM(r); 
                           
-                            l_prime <- evalTypeM(l); 
-                            r_prime <- evalTypeM(r); 
+                            Num l_prime <- evalTypeM(l); 
+                            Num r_prime <- evalTypeM(r); 
                             let potential_eval = l_prime*r_prime in
                              if 0 > potential_eval then Nothing 
-                                else return (potential_eval) }
+                                else return (Num (potential_eval)) }
 
 evalTypeM (Div l r) = do { TNum <- typeofM(l);
                            TNum <- typeofM(r); 
                            
-                           l_prime <- evalTypeM(l); 
-                           r_prime <- evalTypeM(l); 
+                           Num l_prime <- evalTypeM(l); 
+                           Num r_prime <- evalTypeM(l); 
                            if 0 == r_prime then Nothing else
-                               let potential_eval = (div l_prime r_prine) in 
+                               let potential_eval = (div l_prime r_prime) in 
                                 if 0 > potential_eval then Nothing 
-                                    else return (potential_eval) }
+                                    else return (Num (potential_eval)) }
 evalTypeM (And l r) = do { TBool <- typeofM(l); 
                            TBool <- typeofM(r); 
                            
-                           l_prime <- evalTypeM(l); 
-                           r_prime <- evalTypeM(r); 
-                           return (l_prime && r_prime) }
+                           Boolean l_prime <- evalTypeM(l); 
+                           Boolean r_prime <- evalTypeM(r); 
+                           return (Boolean (l_prime && r_prime)) }
 evalTypeM (Leq l r) = do { TNum <- typeofM(l); 
                            TNum <- typeofM(r); 
                            
-                           l_prime <- evalTypeM(l); 
-                           r_prime <- evalTypeM(r); 
-                           return (l_prime <= r_prime); }
+                           Num l_prime <- evalTypeM(l); 
+                           Num r_prime <- evalTypeM(r); 
+                           return (Boolean (l_prime <= r_prime)); }
 evalTypeM (IsZero t) = do { TNum <- typeofM(t);
                              
-                            term <- evalTypeM(t); 
-                            return (0 == term) }
+                            Num term <- evalTypeM(t); 
+                            return (Boolean (0 == term)) }
 evalTypeM (If c t e) = do { TBool <- typeofM(c);
 
-                            condition <- evalTypeM(c);
+                            Boolean condition <- evalTypeM(c);
 
                             then_type <- typeofM(t); 
                             else_type <- typeofM(e); 
                             
                             if (then_type /= else_type) then Nothing else 
                                 if condition then evalTypeM(t) else evalTypeM(e) }
-evalTypeM _ = Nothing
 
 -- Optimizer
 
 optimize :: ABE -> ABE
-optimize (Plus l (Num 0)) = l
-optimize (Plus (Num 0) r) = r
-optimize (Minus l (Num 0)) = l 
-optimize (Mult l (Num 1)) = l
-optimize (Div l (Num 1)) = l
 optimize (IsZero (Num 0)) = (Boolean True)
-optimize (If (Boolean True) t _) = t
-optimize (If (Boolean False) _ e) = e
+optimize (Plus l (Num 0)) = optimize(l)
+optimize (Plus (Num 0) r) = optimize(r)
+optimize (Minus l (Num 0)) = optimize(l) 
+optimize (Mult l (Num 1)) = optimize(l)
+optimize (Div l (Num 1)) = optimize(l)
+optimize (If (Boolean True) t _) = optimize(t)
+optimize (If (Boolean False) _ e) = optimize(e)
+optimize (Plus l r) = (Plus (optimize(l)) (optimize(r)))
+optimize (Minus l r) = (Minus (optimize(l)) (optimize(r)))
+optimize (Mult l r) = (Mult (optimize(l)) (optimize(r)))
+optimize (Div l r) = (Div (optimize(l)) (optimize(r)))
+optimize (And l r) = (And (optimize(l)) (optimize(r)))
+optimize (Leq l r) = (Leq (optimize(l)) (optimize(r)))
+optimize (IsZero t) = (IsZero (optimize(t)))
+optimize (If c t e) = (If (optimize(c)) (optimize(t)) (optimize(e)))
 optimize catch_all = catch_all
 
 interpOptM :: ABE -> Maybe ABE
-interpOptM _ = Nothing
+interpOptM expression = evalTypeM(optimize(expression))
